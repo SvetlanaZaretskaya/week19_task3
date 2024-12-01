a513a55c94f74ef0d825213bc0ee08bf84610078
SvetlanaZaretskaya <svetlanazaretskaya@yandex.ru>
2024-12-01 16:16:49 +0300

    changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const resultContainer = document.getElementById('results');
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
@@ -26,22 +27,37 @@ async function sendData(value1, value2) {
         const data = await response.json();
         console.log(data); 
 
+        data.forEach(element => {
+            const header = document.createElement('p');
+            header.style.fontWeight = 'bold';
+            header.textContent = element.title;
+            resultContainer.appendChild(header);
+
+            const body = document.createElement('p');
+            body.textContent = element.body;
+            resultContainer.appendChild(body);
+        })
+
+        // addResultToPage(value1, value2);
+
+
         addResultToPage(value1, value2);
     } catch (error) {
         console.error('Ошибка:', error);
     }
 }
 
-function addResultToPage(value1, value2) {
-    const resultContainer = document.getElementById('results');
-    const newItem = document.createElement('p');
-    newItem.className = 'result-item';
-    newItem.textContent = value1;
-    resultContainer.appendChild(newItem);
+// function addResultToPage(value1, value2) {
+//     
+//     const newItem = document.createElement('p');
+//     newItem.className = 'result-item';
+//     newItem.textContent = value1;
+//     resultContainer.appendChild(newItem);
 
-    const newItem2 = document.createElement('p');
-    newItem2.className = 'result-item';
-    newItem2.textContent = value2;
-    resultContainer.appendChild(newItem);
+//     const newItem2 = document.createElement('p');
+//     newItem2.className = 'result-item';
+//     newItem2.textContent = value2;
+//     resultContainer.appendChild(newItem);
 
-}
+// }
+
